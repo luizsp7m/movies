916dbd5df385a72ed6f7b326026faed824707cd2
Luiz Oliveira <luizoliveira2808@gmail.com>
2022-03-22 19:44:13 -0300

    Correção

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -6,18 +6,18 @@ interface Props {
   onChangePages: (page: number) => void;
 }
 
-export function Pagination({numberPages, currentPage, onChangePages}: Props) {
+export function Pagination({ numberPages, currentPage, onChangePages }: Props) {
   return (
     <Container>
-      { Array.from(Array(numberPages), (item, index) => (
-        <button 
-        key={index} 
-        className={currentPage === index + 1 ? `selected` : ""}
-        onClick={() => onChangePages(index + 1)}
+      {Array.from(Array(numberPages), (item, index) => (
+        <button
+          key={index}
+          className={currentPage === index + 1 ? `selected` : ""}
+          onClick={() => onChangePages(index + 1 + item)}
         >
-          { index + 1 }
+          {index + 1}
         </button>
-      )) }
+      ))}
     </Container>
   );
 }
